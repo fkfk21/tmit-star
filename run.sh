a89254d215ef9697e20e91659dcbb1ea62b13360
Kenji Fukuda <fk.kenji.21@gmail.com>
2024-09-22 02:11:18 +0900

    add message for debug in run.sh and modify ompl inclusion

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ prefix=()
 suffix=()
 profile_run=false
 if [ "$1" = "profile" ]; then
+  echo "Running with perf profiling..."
   profile_run=true
   prefix+=(perf record --call-graph dwarf -F 99 --)
   shift 1
@@ -12,10 +13,12 @@ if [ "$1" = "profile" ]; then
     return 0
   }
 elif [ "$1" = "debug" ]; then
+  echo "Running with lldb..."
   prefix+=(lldb)
   suffix+=(--)
   shift 1
 elif [ "$1" = "rr" ]; then
+  echo "Running with rr..."
   prefix+=(rr record)
   shift 1
 fi
